3fd6ffdffa82eb723adac1c085c56427f2620103
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-03-27 16:23:07 +0100

    Add project files.

---
+++ ApiOAuthEmpleados/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using ApiOAuthEmpleados.Helpers;
+using ApiOAuthEmpleados.Models;
+using ApiOAuthEmpleados.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiOAuthEmpleados.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private RepositoryHospital repo;
+        //CUANDO GENEREMOS EL TOKEN DEBEMOS INTEGRAR
+        //ALGUNOS DATOS COMO ISSUER Y DEMÁS
+        private HelperActionServicesOAuth helper;
+
+        public AuthController(RepositoryHospital repo,
+            HelperActionServicesOAuth helper)
+        {
+            this.repo = repo;
+            this.helper = helper;
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult>
+            Login(LoginModel model) 
+        {
+            Empleado empleado = await this.repo.LogInEmpleadoAsync(model.UserName,
+                int.Parse(model.Password));
+            if (empleado == null)
+            {
+                return Unauthorized();
+            }
+            else
+            {
+                //DEBEMOS CREAR UNAS CREDENCIALES PARA 
+                //INCLUIRLAS DENTRO DEL TOKEN Y QUE ESTARÁN
+                //COMPUESTAS POR EL SECRET KEY CIFRADO Y EL
+                //TIPO DE CIFRADO QUE INCLUIREMOS EN EL TOKEN
+                SigningCredentials credentials =
+                    new SigningCredentials
+                    (this.helper.GetKeyToken(),
+                    SecurityAlgorithms.HmacSha256);
+                //EL TOKEN SE GENERA CON UNA CLASE
+                //Y DEBEMOS INDICAR LOS DATOS QUE 
+                //ALMACENARÁ EN SU INTERIOR
+                JwtSecurityToken token =
+                    new JwtSecurityToken(
+                        issuer: this.helper.Issuer,
+                        audience: this.helper.Audience,
+                        signingCredentials: credentials,
+                        expires: DateTime.UtcNow.AddMinutes(20),
+                        notBefore: DateTime.UtcNow
+                        );
+                //POR ÚLTIMO, DEVOLVEMOS LA RESPUESTA AFIRMATIVA
+                //CON UN OBJETO QUE CONTENGA EL TOKEN (anónimo)
+                return Ok(new
+                {
+                    response = 
+                    new JwtSecurityTokenHandler()
+                    .WriteToken(token)
+                });
+            }
+        }
+    }
+}
---
+++ ApiOAuthEmpleados/Program.cs
@@ -0,0 +1,58 @@
+using ApiOAuthEmpleados.Data;
+using ApiOAuthEmpleados.Helpers;
+using ApiOAuthEmpleados.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//**************************************************************************************//
+//                                      SEGURIDAD
+//**************************************************************************************//
+//CREAMOS UNA INSTANCIA DE NUESTRO HELPER
+HelperActionServicesOAuth helper =
+    new HelperActionServicesOAuth(builder.Configuration);
+//ESTA INSTANCIA SOLAMENTE DEBEMOS CREARLA UNA VEZ
+//PARA QUE NUESTRA APLICACION PUEDA VALIDAR CON TODO LO QUE HA CREADO
+builder.Services.AddSingleton<HelperActionServicesOAuth>(helper);
+//HABILITAMOS LA SEGURIDAD UTILIZANDO LA CLASE HELPER
+builder.Services.AddAuthentication(helper.GetAuthenticateSchema())
+    .AddJwtBearer(helper.GetJwtBearerOptions());
+//**************************************************************************************//
+
+// CADENA DE CONEXIÓN A LA BASE DE DATOS DE AZURE
+string connectionString = builder.Configuration.GetConnectionString("SqlAzure");
+
+// INYECCIÓN AL CONTEXTO DE LA CADENA DE CONEXIÓN
+builder.Services.AddDbContext<HospitalContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// REPOSITORIO
+builder.Services.AddTransient<RepositoryHospital>();
+
+// RESTO DE SERVICIOS
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    
+}
+
+app.MapOpenApi();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Seguridad Empleados");
+    options.RoutePrefix = "";
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();// <- SEGURIDAD
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
